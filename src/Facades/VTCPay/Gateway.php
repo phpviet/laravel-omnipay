0fb7e3d39bd091cefaa068282249151c0a465059
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-30 01:32:37 +0700

    Updated VTCPay gateway.

--- src/Facades/VTCPay/Gateway.php
+++ src/Facades/VTCPay/Gateway.php
@@ -11,6 +11,9 @@ namespace PHPViet\Laravel\Omnipay\Facades\VTCPay;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Omnipay\VtcPay\Message\PurchaseRequest purchase(array $options = [])
+ * @method static \Omnipay\VtcPay\Message\CompletePurchaseRequest completePurchase(array $options = [])
+ *
  * @author Vuong Minh <vuongxuongminh@gmail.com>
  * @since 1.0.0
  */
@@ -21,6 +24,20 @@ class Gateway extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        return static::$app['omnipay']->gateway('vtcpay');
+        return static::$app['omnipay']->gateway('VTCPay');
+    }
+
+    /**
+     * Create a request to accept notification.
+     *
+     * This is an alias of [[acceptNotification()]] for sync with style standard of another gateway.
+     *
+     * @param  array  $options
+     *
+     * @return \Omnipay\VtcPay\Message\AcceptNotificationRequest
+     */
+    public function notification(array $options = [])
+    {
+        return static::getFacadeAccessor()->acceptNotification($options);
     }
 }
