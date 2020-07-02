e0f3a14b7c135d6e2c70c881404bb5d6ffaf1d06
Minh Vuong <vuongxuongminh@gmail.com>
2020-07-02 23:54:34 +0700

    Add complete purchase middleware.

---
+++ src/Middlewares/CompletePurchaseMiddleware.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @link https://github.com/phpviet/laravel-omnipay
+ *
+ * @copyright (c) PHP Viet
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace PHPViet\Laravel\Omnipay\Middlewares;
+
+use Closure;
+use Omnipay\Common\AbstractGateway;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.1.0
+ */
+class CompletePurchaseMiddleware
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, string $gateway, $successHandle, $failureHandle = null)
+    {
+        /** @var AbstractGateway $gateway */
+        $gateway = app('omnipay')->gateway($gateway);
+
+        if (!$gateway->supportsCompletePurchase()) {
+            throw new \InvalidArgumentException('Gateway configured not support complete purchase method!');
+        }
+
+        $response = $gateway->completePurchase()->send();
+
+        if ($response->isSuccessful()) {
+            return app()->call($successHandle, [$response]);
+        }
+
+        if ($failureHandle) {
+            return app()->call($failureHandle, [$response]);
+        }
+
+        throw new BadRequestHttpException('Bad request');
+    }
+}
